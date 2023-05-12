c9168a1942eb0a5af49e6fc93809cf3bf1d39553
sergey <szaprudnov@hotmail.com>
2023-05-12 19:34:31 +0300

    adding get_companies_and_vacancies_count method

--- base/db_manager.py
+++ base/db_manager.py
@@ -15,3 +15,15 @@ class DBManager:
         if self._connection is not None:
             self._connection.close()
 
+    def get_companies_and_vacancies_count(self):
+        self.connect()
+        cur = self._connection.cursor()
+        cur.execute("SELECT e.name AS company_name, COUNT(v.vacancy_id) AS vacancies_count"
+                    "FROM employers e LEFT JOIN vacancies v ON e.employer_id = v.employer_id"
+                    "GROUP BY e.name"
+                    "ORDER BY vacancies_count DESC;")
+        rows = cur.fetchall()
+        for r in rows:
+            print(f"{r}: {r} vacancies")
+        cur.close()
+        self.close()

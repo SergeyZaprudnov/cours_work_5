1f968cdc1be4aac8cf4299e23b19085407a8d247
sergey <szaprudnov@hotmail.com>
2023-05-12 18:14:15 +0300

    adding initialization and connection method

--- base/db_manager.py
+++ base/db_manager.py
@@ -2,3 +2,11 @@ import psycopg2
 from base.conf_parser import config
 
 class DBManager:
+    def __init__(self, database_name: str):
+        self._database = database_name
+        self._connection = None
+
+    def connect(self):
+        params = config()
+        params['database'] = self._database
+        self._connection = psycopg2.connect(**params)

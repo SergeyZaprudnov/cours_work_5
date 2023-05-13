792b2cd3d29b3db046020610fb20fb5c177ae516
sergey <szaprudnov@hotmail.com>
2023-05-13 08:32:20 +0300

    adding a function and changing the import

--- base/insert_db.py
+++ base/insert_db.py
@@ -1,3 +1,24 @@
 import psycopg2
 from src.hh_api_key import HHApi
-from base.conf_parser import config
+from base.config import user, password
+
+
+def inert_data(dbname: str, search_query) -> None:
+    conn = psycopg2.connect(database=dbname, user=user, password=password, host="localhost", port="5432")
+    cur = conn.cursor()
+    hh_api = HHApi()
+    employers_data, vacancies_data = hh_api.get_vacancies(search_query)
+    for emp in employers_data:
+        cur.execute("INSERT INTO employers (employers_id, name, url) VALUES (%s, %s, %s)",
+                    (emp['employer_id'], emp['name'], emp['url']))
+    for vac in vacancies_data:
+        cur.execute(
+            "INSERT INTO vacancies"
+            "(employer_id, name, description, area, url, salary_from, salary_to, currency, published_at)"
+            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", vac['employer_id'], vac['vacancy_name'],
+            vac['description'], vac['area'], vac['url'], vac['salary_from'], vac['salary_to'], vac['currency'],
+            vac['published_at'])
+
+        conn.commit()
+        cur.close()
+        conn.close()

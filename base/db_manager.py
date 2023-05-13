744d788d8ad5b5118ad529fae7d77666a9d146ab
sergey <szaprudnov@hotmail.com>
2023-05-13 08:47:43 +0300

    alteration

--- base/db_manager.py
+++ base/db_manager.py
@@ -5,20 +5,11 @@ from base.config import user, password
 class DBManager:
     def __init__(self, database_name: str):
         self._database = database_name
-        self._connection = None
-
-    def connect(self):
-        params = config()
-        params['database'] = self._database
-        self._connection = psycopg2.connect(**params)
-
-    def close(self):
-        if self._connection is not None:
-            self._connection.close()
 
     def get_companies_and_vacancies_count(self):
-        self.connect()
-        cur = self._connection.cursor()
+
+        conn = psycopg2.connect(host="localhost", database=self._database, user=user, password=password)
+        cur = conn.cursor()
         cur.execute("SELECT e.name AS company_name, COUNT(v.vacancy_id) AS vacancies_count"
                     "FROM employers e LEFT JOIN vacancies v ON e.employer_id = v.employer_id"
                     "GROUP BY e.name"
@@ -27,11 +18,11 @@ class DBManager:
         for r in rows:
             print(f"{r}: {r} vacancies")
         cur.close()
-        self.close()
+        conn.close()
 
     def get_all_vacansies(self):
-        self.connect()
-        cur = self._connection.cursor()
+        conn = psycopg2.connect(host="localhost", database=self._database, user=user, password=password)
+        cur = conn.cursor()
         cur.execute(
             "SELECT e.name AS company_name, v.name AS vacancy_name, v.salary_from, v.salary_to, v.currency, v.url"
             "FROM employers e"
@@ -40,30 +31,30 @@ class DBManager:
         for r in rows:
             print(r)
         cur.close()
-        self.close()
+        conn.close()
 
     def get_avg_salary(self):
-        self.connect()
-        cur = self._connection.cursor()
+        conn = psycopg2.connect(host="localhost", database=self._database, user=user, password=password)
+        cur = conn.cursor()
         cur.execute("SELECT AVG((salary_from + salary_to) / 2) as average_salary FROM vacancies;")
         rows = cur.fetchall()
         print(f"Средняя заработная плата от: {rows}")
         cur.close()
-        self.close()
+        conn.close()
 
     def get_vacancies_with_higher_salary(self):
-        self.connect()
-        cur = self._connection.cursor()
+        conn = psycopg2.connect(host="localhost", database=self._database, user=user, password=password)
+        cur = conn.cursor()
         cur.execute("SELECT * FROM vacancies WHERE salary_from > (SELECT AVG(salary_from) FROM vacancies)")
         rows = cur.fetchall()
         for r in rows:
             print(f"id: {r[0]} Вакансия: {r[2], r[3], r[4], r[5], r[6]}")
         cur.close()
-        self.close()
+        conn.close()
 
     def get_vacancies_with_keyword(self, keyword):
-        self.connect()
-        cur = self._connection.cursor()
+        conn = psycopg2.connect(host="localhost", database=self._database, user=user, password=password)
+        cur = conn.cursor()
         sql_query = "SELECT * FROM vacancies WHERE name LIKE %s"
         search_params = (f'%{keyword}%',)
         cur.execute(sql_query, search_params)
@@ -71,4 +62,4 @@ class DBManager:
         for r in result:
             print(f"id: {r[0]} Вакансия: {r[2], r[3], r[4], r[5], r[6]}")
         cur.close()
-        self.close()
+        conn.close()
